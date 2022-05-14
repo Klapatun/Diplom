f136a18e0a51c26861496310ef826f8a9c9a1f06
Klapatun Anton <KlapatunA@mail.ru>
2022-05-15 05:10:49 +0700

    changed

--- nvprof_start.bat
+++ nvprof_start.bat
@@ -1,2 +1,3 @@
-echo "nvprof start\n"
-echo "check time of cuBLAS"
+nvprof --log-file ./cuBLAS.log ./test1/test1/x64/Release/test1.exe
+nvprof --log-file ./cuTensor.log  ./test_cutensor/x64/Debug/test_cutensor.exe
+nvprof --log-file ./thrust.log  ./test2_thrust/x64/Debug/test2_thrust.exe
